736064e9eadbb93387a1bdcfacd31e4f7575db04
xobaib <xobaib2001.m@gmail.com>
2024-12-23 14:36:33 +0330

    03-03:Secure Secret App Data with Vite Environment Variables

--- src/lib/supabaseClient.ts
+++ src/lib/supabaseClient.ts
@@ -2,6 +2,8 @@ import { createClient } from '@supabase/supabase-js';
 
 // Create a single supabase client for interacting with your database
 export const supabase = createClient(
-  'https://pgudmwltmlqrejghxwzq.supabase.co',
-  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InBndWRtd2x0bWxxcmVqZ2h4d3pxIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzQ5MzY4MDksImV4cCI6MjA1MDUxMjgwOX0.GGLgKjh4F2WXml-efXsOHpnlEi05Hfk45fcOB89172E',
+  import.meta.env.VITE_SUPABASE_URL,
+  import.meta.env.VITE_SUPABASE_KEY,
 );
+
+console.log(import.meta.env.VITE_SUPER_SECRET_KEY);
