9a5d9932b9d4a5873af35ad5d245342a3c48ca15
xobaib <xobaib2001.m@gmail.com>
2024-12-25 10:53:15 +0330

    03-09:Set Up Supabase JavaScript Client in Node Environment

--- database/seed.js
+++ database/seed.js
@@ -1,6 +1,9 @@
+/* eslint-env node */
+
 import { fakerEN_US as faker } from '@faker-js/faker';
+import { createClient } from '@supabase/supabase-js';
 
-const personName = faker.person.fullName();
-const personBio = faker.person.bio();
+// Create a single supabase client for interacting with your database
+const supabase = createClient(process.env.VITE_SUPABASE_URL, process.env.SERVICE_ROLE_KEY);
 
-console.log(`Hi my name is ${personName}. and I am a ${personBio}`);
+console.log(supabase);
