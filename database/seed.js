c1dfdcddf9458fb673047530c7cbee776f19eefb
xobaib <xobaib2001.m@gmail.com>
2024-12-25 11:35:23 +0330

    03-11:Insert Bulk Entries Into Supabase Database

--- database/seed.js
+++ database/seed.js
@@ -6,15 +6,20 @@ import { createClient } from '@supabase/supabase-js';
 // Create a single supabase client for interacting with your database
 const supabase = createClient(process.env.VITE_SUPABASE_URL, process.env.SERVICE_ROLE_KEY);
 
-async function seedProject() {
-  const name = faker.lorem.word(3);
+async function seedProject(numEnries) {
+  const projects = [];
 
-  await supabase.from('projects').insert({
-    name: name,
-    slug: name.toLowerCase(/ /g, '-'),
-    status: faker.helpers.arrayElement(['in-progress', 'completed']),
-    collaborators: faker.helpers.arrayElements([1, 2, 3]),
-  });
+  for (let i = 0; i < numEnries; i++) {
+    const name = faker.lorem.word(3);
+
+    projects.push({
+      name: name,
+      slug: name.toLowerCase(/ /g, '-'),
+      status: faker.helpers.arrayElement(['in-progress', 'completed']),
+      collaborators: faker.helpers.arrayElements([1, 2, 3]),
+    });
+  }
+  await supabase.from('projects').insert(projects);
 }
 
-await seedProject();
+await seedProject(10);
