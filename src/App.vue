803c3f38ed612090bbe608326832697a873cf191
xobaib <xobaib2001.m@gmail.com>
2024-12-28 10:19:28 +0330

    04-02:Prepare the Vue App Layout with TailwindCSS

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,12 @@
-<script setup lang="ts">
-import { supabase } from '@/lib/supabaseClient';
-
-// console.log(supabase);
-</script>
+<script setup lang="ts"></script>
 
 <template>
+  <nav class="h-16 border-b bg-muted/40 flex justify-between items-center px-6">
+    <form class="w-full max-w-96">
+      <input type="text" placeholder="Search ..." class="w-full bg-background pl-8" />
+    </form>
+    <div class="w-8 h-8 rounded-full bg-white"></div>
+  </nav>
   <main>
     <RouterView />
   </main>
