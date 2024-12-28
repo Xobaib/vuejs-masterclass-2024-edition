a9118feffe681b529fe54954206957d806fdc95a
xobaib <xobaib2001.m@gmail.com>
2024-12-28 11:16:06 +0330

    04-04:Use Lucide Icons with Iconify and Vue

--- src/App.vue
+++ src/App.vue
@@ -14,6 +14,18 @@ import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar';
 <template>
   <nav class="h-16 border-b bg-muted/40 flex justify-between items-center px-6">
     <form class="w-full max-w-96">
+      <svg xmlns="http://www.w3.org/2000/svg" width="22" height="22" viewBox="0 0 24 24">
+        <g
+          fill="none"
+          stroke="#fff"
+          stroke-linecap="round"
+          stroke-linejoin="round"
+          stroke-width="2"
+        >
+          <circle cx="11" cy="11" r="8" />
+          <path d="m21 21l-4.3-4.3" />
+        </g>
+      </svg>
       <Input type="text" placeholder="Search ..." class="w-full bg-background pl-8" />
     </form>
 
