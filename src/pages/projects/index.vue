be126d5bdf1ae7b9aa882d608b3b65cb1f27640a
xobaib <xobaib2001.m@gmail.com>
2024-12-31 18:51:09 +0330

    05-04:Use Vue.js Suspense Component to Handle Async Dependencies

--- src/pages/projects/index.vue
+++ src/pages/projects/index.vue
@@ -6,16 +6,15 @@ import { RouterLink } from 'vue-router';
 
 let projects = ref<Tables<'projects'>[] | null>(null);
 
-// We're using an IIFE function to get the data as soon as possible.(in setup phase)
-(async () => {
+async function getProjects() {
   const { data, error } = await supabase.from('projects').select();
 
   if (error) console.log(error);
 
   projects.value = data;
+}
 
-  // console.log('Projects: ', projects.value);
-})();
+await getProjects();
 
 const columns: ColumnDef<Tables<'projects'>>[] = [
   {
